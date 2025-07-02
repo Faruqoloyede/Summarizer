64a02ce84810fd3990016335788a8b2569e5ca5e
Faruqoloyede <oloyedefaruq2@gmail.com>
2025-07-02 19:55:08 +0100

    change subtext in hero section and added animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,21 +1,42 @@
-import Button from "./Button"
-
+import { motion } from "framer-motion";
+import Button from "./Button";
 
 const Hero = () => {
   return (
-    <section className='relative h-screen bg-[#061212]'>
-        <div className="container mx-auto px-4 pt-32 flex max-lg:flex-col items-center justify-between gap-10">
-            <div className="flex-1 text-center lg:text-left">
-                <h1 className="font-poppins font-bold text-[32px] sm:text-[40px] lg:text-[48px] leading-tight text-white capitalize">AI-Powered Notes.Organize and Summarize in Seconds</h1>
-                <p className="text-base text-para my-4 max-w-lg mx-auto lg:mx-0">Let AI organize & summarize your notes, saving you time and boosting productivity</p>
-                 <Button title="Get Started" to="/login" />
-            </div>
-            <div className="flex-1 flex justify-center">
-                <img src="/hero.png" alt="hero" className="w-[300px] sm:w-[360px] md:w-[440px] lg:w-[500px] h-auto object-contain" />
-            </div>
-        </div>
-    </section>
-  )
-}
+    <section className="relative h-screen bg-[#061212]">
+      <div className="container mx-auto px-4 pt-32 flex max-lg:flex-col items-center justify-between gap-10">
+        {/* Text */}
+        <motion.div
+          className="flex-1 text-center lg:text-left"
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <h1 className="font-poppins font-bold text-[32px] sm:text-[40px] lg:text-[48px] leading-tight text-white capitalize">
+            AI-Powered Notes. Organize and Summarize in Seconds
+          </h1>
+          <p className="text-base text-para my-4 max-w-lg mx-auto lg:mx-0">
+           Upload notes, documents, or PDFs and let AI generate concise, readable summaries in seconds. Save time. Stay focused. Learn smarter.
+          </p>
+          <Button title="Get Started" to="/login" />
+        </motion.div>
 
-export default Hero
+        {/* Image */}
+        <motion.div
+          className="flex-1 flex justify-center"
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <img
+            src="/hero.png"
+            alt="hero"
+            className="w-[300px] sm:w-[360px] md:w-[440px] lg:w-[500px] h-auto object-contain"
+          />
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
